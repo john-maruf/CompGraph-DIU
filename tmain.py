d949d4dede9f2d0579f7e72a7226905dd751d5fe
John Maruf <marufakterjon@gmail.com>
2026-03-04 10:11:09 +0600

    Update tmain.py

--- tmain.py
+++ tmain.py
@@ -5,7 +5,11 @@ from OpenGL.GLU import *
 
 # Scale variables
 scaleX, scaleY = 1.0, 1.0
-Sx, Sy = 0.5, 1.6 
+Sx, Sy = 0.5, 1.6  
+
+# Translation variables
+posX, posY = 0.0, 0.0
+move_step = 20.0
 
 
 def draw_triangle():
@@ -13,23 +17,30 @@ def draw_triangle():
     
     glBegin(GL_TRIANGLES)
     glVertex2f(250, 250)  # Top
-    glVertex2f(100, 100)    # Bottom Left
-    glVertex2f(400, 100)    # Bottom Right
+    glVertex2f(100, 100)  # Bottom Left
+    glVertex2f(400, 100)  # Bottom Right
     glEnd()
 
 
 def display():
     glClear(GL_COLOR_BUFFER_BIT)
     glLoadIdentity()
+
+    # Apply translation first
+    glTranslatef(posX, posY, 0.0)
+
+    # Apply scaling
     glScalef(scaleX, scaleY, 1.0)
+
     draw_triangle()
+
     glutSwapBuffers()
 
 
 def keyboard(key, x, y):
-    global scaleX, scaleY
+    global scaleX, scaleY, posX, posY
 
-
+    # Scaling
     if key == b'+':
         scaleX += Sx
         scaleY += Sy
@@ -38,8 +49,18 @@ def keyboard(key, x, y):
         scaleX -= Sx
         scaleY -= Sy
 
-        
+    # Movement
+    if key == b'w':  # Up
+        posY += move_step
 
+    if key == b's':  # Down
+        posY -= move_step
+
+    if key == b'a':  # Left
+        posX -= move_step
+
+    if key == b'd':  # Right
+        posX += move_step
 
     glutPostRedisplay()
 
@@ -50,24 +71,22 @@ def initiate():
     glMatrixMode(GL_PROJECTION)
     glLoadIdentity()
     glOrtho(0.0, 500, 0.0, 500, 0.0, 1.0)
-    glMatrixMode(GL_MODELVIEW)  # FIX 3: Switch back to modelview
+    glMatrixMode(GL_MODELVIEW)
 
 
 glutInit()
 glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
 glutInitWindowSize(500, 500)
 glutInitWindowPosition(100, 100)
-glutCreateWindow(b"Example 2 - Scale 2D Triangle")
-
+glutCreateWindow(b"Example 2 - Scale & Move 2D Triangle")
 
 initiate()
-
 
 glutDisplayFunc(display)
 glutKeyboardFunc(keyboard)
 
-
 print("Press + to scale up")
-
+print("Press - to scale down")
+print("Use W A S D to move the triangle")
 
 glutMainLoop()
