d5f4324fab611132cea5c05187fc1b7b3287d944
John Maruf <marufakterjon@gmail.com>
2026-03-04 09:41:26 +0600

    Add OpenGL program to draw and scale a triangle

---
+++ tmain.py
@@ -0,0 +1,68 @@
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+
+
+# Scale variables
+scaleX, scaleY = 1.0, 1.0
+Sx, Sy = 0.1, 0.1 
+
+
+def draw_triangle():
+    glColor3f(1.0, 0.6, 0.2)  # Orange
+    
+    glBegin(GL_TRIANGLES)
+    glVertex2f(250, 250)  # Top
+    glVertex2f(100, 100)    # Bottom Left
+    glVertex2f(400, 100)    # Bottom Right
+    glEnd()
+
+
+def display():
+    glClear(GL_COLOR_BUFFER_BIT)
+    glLoadIdentity()
+    glScalef(scaleX, scaleY, 1.0)
+    draw_triangle()
+    glutSwapBuffers()
+
+
+def keyboard(key, x, y):
+    global scaleX, scaleY
+
+
+    if key == b'+':
+        scaleX += Sx
+        scaleY += Sy
+
+
+
+    glutPostRedisplay()
+
+
+def initiate():
+    glClearColor(0.0, 0.0, 0.0, 1.0)
+    glViewport(0, 0, 500, 500)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0.0, 500, 0.0, 500, 0.0, 1.0)
+    glMatrixMode(GL_MODELVIEW)  # FIX 3: Switch back to modelview
+
+
+glutInit()
+glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
+glutInitWindowSize(500, 500)
+glutInitWindowPosition(100, 100)
+glutCreateWindow(b"Example 2 - Scale 2D Triangle")
+
+
+initiate()
+
+
+glutDisplayFunc(display)
+glutKeyboardFunc(keyboard)
+
+
+print("Press + to scale up")
+
+
+glutMainLoop()
